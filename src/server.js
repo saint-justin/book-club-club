41f0a85d1f17f2b87ec64e52eae45ef765d5c7d3
Justin Vaughn <jtv6445@g.rit.edu>
2020-09-26 19:25:44 -0400

    Setting up server.js structure

--- src/server.js
+++ src/server.js
@@ -1,29 +1,52 @@
-const http = requires('http');
+const http = require('http');
 const url = require('url');
-const query = require('query');
+const query = require('querystring');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
-const urlStruct = {
-  '/': htmlHandler.getClient,
-  '/style.css': htmlHandler.getStyle,
-  '/getUsers': jsonHandler.getUsers,
+
+// Handles a request by passing in the parsedUrl and params to needed functions
+const handlePutRequest = (req, res, parsedUrl, callback) => {
+  const body = [];
+  
+  // If we error out, throw it to the user
+  req.on('error', (err) => {
+    console.dir(err);
+    res.statusCode = 400;
+    res.end();
+  });
+
+  // Build up the request query
+  req.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  // Once we reach the end of the request, use the provided callback w/ parsed query params
+  req.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = query.parse(bodyString);
+    callback(req, res, parsedUrl, bodyParams);
+
+    // Debug output
+    console.log(`PATH: ${parsedUrl.pathname}    METHOD: ${req.method}    PARAMS: []`);
+  });
 };
 
+// Handles incoming requests to the server
 const onRequest = (req, res) => {
   const parsedUrl = url.parse(req.url);
-
-  // Prints info about the request
-  console.log(`PATH: '${parsedUrl.pathname}'    METHOD: ${req.method}`);
-
-  // Handle cases for get, post, and head requests
-  // if ((req.method === 'GET' || req.method === 'HEAD') && urlStruct[parsedUrl.pathname]) {
-  //   urlStruct[parsedUrl.pathname](req, res);
-  // } else if (req.method === 'POST') {
-  //   handlePost(req, res, parsedUrl);
-  // } else {
-  //   jsonHandler.missing(req, res);
-  // }
+  switch (req.method) {
+    case 'GET':
+      console.log(`PATH: ${parsedUrl.pathname}    METHOD: ${req.method}    PARAMS: []`);
+      break;
+    case 'PUT':
+      handlePutRequest(req, res, parsedUrl, () => { });
+      break;
+    default:
+      console.log(`ERR: Method type ${req.method} not acceptable`);
+      break;
+  }
+  handlePutRequest(req, res, parsedUrl, () => ({}));
 };
 
 http.createServer(onRequest).listen(port);
-// console.log(`Listening on 127.0.0.1:${port}`);
+console.log(`Listening on 127.0.0.1:${port}`);
